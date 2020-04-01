f4f27f2baffb759015f09bb2c63249da4fe792a0
unknown <macjay314@gmail.com>
2020-04-01 11:59:56 -0700

    Attempt to add exception handling.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 var vowels = ["a", "e", "i", "o", "u"];
 
+var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
+
 var pigLatin = [];
 
 var vowelStart = function(sent){
@@ -7,43 +9,41 @@ var vowelStart = function(sent){
 
   sentenceArray.forEach(function(word){
     var pigWord = "";
-
-      if (vowels.includes(word[0]) === false){
-      var indexC = 0;
+      if(vowels.includes(word[0]) === false && consonants.includes(word[0]) === false){
+        pigLatin.push(word);
+      }
+      if(word.charCodeAt([0]) < 65 || 90 < word.charCodeAt([0]) < 97 || word.charCodeAt([0]) > 122){
+        pigLatin.push(word);
+      }
+      // all letters ===  65-90 & 97-122
+      else if (vowels.includes(word[0]) === false){
+        var indexC = 0;
       while (vowels.includes(word[indexC]) === false) {
         indexC++;        
-      }
-      alert(word[indexC]);
-
-      if(word[indexC-1] === "q" && word[indexC] === "u"){
-        alert("q has been encountered");
+      }      
+        if(word[indexC-1] === "q" && word[indexC] === "u"){
         pigWord = word.slice(indexC+1);
         pigWord = pigWord.concat(word.slice(0, indexC+1) + "ay");
         pigLatin.push(pigWord);
-      } 
-      
-      
-      else {
+      } else {
         pigWord = word.slice(indexC);
         pigWord = pigWord.concat(word.slice(0, indexC) + "ay");
         pigLatin.push(pigWord);
       }
     }
-    else { //(vowels.includes(word[0])) 
+    else { 
       pigWord = word.concat("way");
       pigLatin.push(pigWord);  
     }    
   });
 
-  
-  return pigLatin;
+  // function isLetter(str) {
+  //   return str.length === 1 && str.match(/[a-z]/i);
+  // }
+
+
+  return pigLatin.join(" ");
 }
-
-//for ()
-
-
-
-
 
 $(document).ready(function () {
   $("#formOne").submit(function(event) {
