86af6565a710883408edc698c63be5b4f0b78930
unknown <macjay314@gmail.com>
2020-04-01 10:46:01 -0700

    add javascript to pass consonant spec

--- js/scripts.js
+++ js/scripts.js
@@ -6,11 +6,24 @@ var vowelStart = function(sent){
   var sentenceArray = sent.split(' ');
 
   sentenceArray.forEach(function(word){
-    if (vowels.includes(word[0])) {
-      var pigWord = word.concat("way");
+    var pigWord = "";
+    if (vowels.includes(word[0]) === false){
+      var indexC = 0;
+      while (vowels.includes(word[indexC]) === false) {
+        
+        indexC++;
+      }
+      pigWord = word.slice(indexC);
+      pigWord = pigWord.concat(word.slice(0, indexC) + "ay");
       pigLatin.push(pigWord);
     }
+    else { //(vowels.includes(word[0])) 
+      pigWord = word.concat("way");
+      pigLatin.push(pigWord);  
+    }    
   });
+
+  
   return pigLatin;
 }
 
